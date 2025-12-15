fcdcca86beac0fb6c2b4c9ef74375d34b1e2df50
Riskihekabuzha <serepetsantuy1215@gmail.com>
2025-12-15 16:38:27 +0700

    update HEADER

--- resources/views/dashboard/peserta/magang/components/header.blade.php
+++ resources/views/dashboard/peserta/magang/components/header.blade.php
@@ -2,7 +2,9 @@
     {{-- Tombol Toggle Sidebar --}}
     <button class="p-2 rounded-lg border border-blue-400/30 text-white hover:bg-white/10 transition"
             @click="sidebarOpen = !sidebarOpen">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
     </button>
 
     {{-- Dropdown Profil --}}
